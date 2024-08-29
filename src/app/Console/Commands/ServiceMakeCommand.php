27cc8253a5cc3a2683bd5bc7941fe8e60a6a8516
Aushraful Alam <aushraful@gmail.com>
2024-08-30 00:11:19 +0600

    :pencil2: Add: Custom service:make command.

---
+++ src/app/Console/Commands/ServiceMakeCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+
+class ServiceMakeCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new service';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     */
+    protected function buildClass($name): string|FileNotFoundException
+    {
+        $stub = $this->files->get($this->getStub());
+
+        return $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
+    }
+
+    /**
+     * Replace the class & model name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     */
+    protected function replaceClass($stub, $name): string
+    {
+        $class = str_replace($this->getNamespace($name) . '\\', '', $name);
+
+        $model = str_replace("Service", "", $class);
+
+        return str_replace(array('ClassName', 'ModelName'), array($class, $model), $stub);
+    }
+
+    /**
+     * Get the stub file for the generator.
+     */
+    protected function getStub(): string
+    {
+        return __DIR__ . '/stubs/service.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\Services';
+    }
+}
