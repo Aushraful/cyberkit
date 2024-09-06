3bfd728e381ac1333e26ee45d674f386da484be5
Aushraful Alam <aushraful@gmail.com>
2024-09-06 23:10:00 +0600

    :up: Update: bind verification repository and interface in service provider.

--- src/app/Providers/CustomProviders/RepositoryServiceProvider.php
+++ src/app/Providers/CustomProviders/RepositoryServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers\CustomProviders;
 
 use App\Repositories\Contracts\AuthenticationInterface;
+use App\Repositories\Contracts\VerificationInterface;
 use App\Repositories\Eloquent\AuthenticationRepository;
+use App\Repositories\Eloquent\VerificationRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -23,5 +25,8 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         // Bind AuthenticationRepository to AuthenticationInterface
         $this->app->bind(AuthenticationInterface::class, AuthenticationRepository::class);
+
+        // Bind VerificationRepository to VerificationInterface
+        $this->app->bind(VerificationInterface::class, VerificationRepository::class);
     }
 }
